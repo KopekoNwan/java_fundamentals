cf4df36243b78f16ae42ec95b65047a99a6870ef
root <root@KopekoNWan>
2024-05-06 09:23:40 +0800

    Finish

--- src/labs_examples/datatypes_operators/labs/Exercise_02.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_02.java
@@ -24,6 +24,14 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // write code here
+        int num1 = 10;
+        double num2 = num1;
+        System.out.println(num2);
+
+        double num3 = 10.5;
+        int num4 = (int) num3;
+        System.out.println(num4);
+
 
     }
 }
