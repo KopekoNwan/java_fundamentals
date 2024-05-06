0fa64d9e80a173472df10e055211eb7d1cde6056
root <root@KopekoNWan>
2024-05-06 08:15:35 +0800

    Answered

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -12,10 +12,34 @@ public class Exercise_04 {
 
     public static void main(String[] args) {
 
+        // Different Data types with values
+
+        // Numeric types
+        // floating-point numbers
+        double doubleNum = 10.12345;
+        float floatNum = 3.14f;
+
+        //integers
+        byte byteNum = 127;
+        long longNum = 123456789L;
+        short shortNum = 32767;
         int i = 1;
+        char charType = 'C';
+
+        // boolean type
+        boolean booleanType = true;
+
         System.out.println("int i is: " + i);
+        System.out.println("double = " + doubleNum);
+        System.out.println("float = " + floatNum);
+        System.out.println("byte = " + byteNum);
+        System.out.println("long = " + longNum);
+        System.out.println("short = " + shortNum);
+        System.out.println("char = " + charType);
+        System.out.println("boolean = " + booleanType);
 
         // write your code below
+
 
     }
 
